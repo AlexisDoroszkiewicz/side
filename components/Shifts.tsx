23f60d4d3e43cd532ce3aefd4c33808eee610ab6
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-06-02 16:57:54 +0200

    order w/ state setup

--- components/Shifts.tsx
+++ components/Shifts.tsx
@@ -12,8 +12,8 @@ export default function Shifts({shifts, ...props}) {
 };
 
 const container = css`
-    display: flex;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(min(100%, 7.125rem), 1fr));
     gap: 1rem;
 `
 
@@ -30,17 +30,22 @@ const Shift = ({shift, index, ...props}) => {
         if ((dayjs() > dayjs(shift.start)) && (shift.filledSlots < shift.slots)) setFailing(true);
     }, [])
 
-    const start = dayjs(shift.start).format('DD/MM/YYYY HH:mm');
-    const end = dayjs(shift.end).format('DD/MM/YYYY HH:mm');
+    const startDay = dayjs(shift.start).format('DD/MM/YYYY');
+    const endDay = dayjs(shift.end).format('DD/MM/YYYY');
+
+    const startHour = dayjs(shift.start).format('HH:mm')
+    const endHour = dayjs(shift.end).format('HH:mm')
 
     return (
         <div css={shiftContainer} {...props}>
             <small>
-                <p>Du {start}</p>
-                <p>Au {end}</p>
+                <p>{startDay == endDay ? startDay : `Du ${startDay} au ${endDay}`}</p>
+                <p>{`${startHour} > ${endHour}`}</p>
             </small>
-            <small>
-                <p>Slots {shift.filledSlots}/{shift.slots} {shift.slots == shift.filledSlots ? "✅" : "❌"}</p>
+            <small css={css`display: flex; align-items: center;`}>
+                <p>Slots {shift.filledSlots}/{shift.slots}</p> {shift.slots == shift.filledSlots ? 
+                <svg css={css`fill: var(--green);`} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path id="checkmark-a" d="M10.67 15.82l-3.48-3.39c-.25-.23-.25-.45 0-.68l.68-.69c.25-.23.49-.23.71 0l2.43 2.4 5.4-5.29c.23-.23.47-.23.72 0l.68.65c.25.23.25.46 0 .69l-6.46 6.34c-.23.2-.46.2-.68-.03z"></path></svg> : 
+                <svg css={css`fill: var(--red);`} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M15.89 8.818a.5.5 0 010 .707L13.414 12l2.474 2.476a.5.5 0 010 .707l-.707.707a.5.5 0 01-.707 0L12 13.414l-2.476 2.475a.5.5 0 01-.707 0l-.707-.707a.5.5 0 010-.707l2.475-2.476-2.475-2.474a.5.5 0 010-.707l.707-.707a.5.5 0 01.707 0l2.476 2.474 2.474-2.474a.5.5 0 01.707 0l.707.707z" fillRule="nonzero"></path></svg>}
             </small>
         </div>
     )
@@ -48,6 +53,5 @@ const Shift = ({shift, index, ...props}) => {
 
 const shiftContainer = css`
     border: 1px solid black;
-    width: fit-content;
     padding: 1em;
 `
--- components/Task.tsx
+++ components/Task.tsx
@@ -8,6 +8,8 @@ export const Context = createContext([]);
 export default function Task({task, ...props}) {
     const {company, details, selection, shifts} = task;
 
+    const [failing, setFailing] = useState(false); 
+    const [short, setShort] = useState(false);
     const [closable, setClosable] = useState(false);
 
     // skip closed tasks ❌
@@ -15,30 +17,30 @@ export default function Task({task, ...props}) {
     // if task has no shift, or all shifts are ended, set closable
     useEffect(() => {
         if (!shifts || shifts.every(shift => dayjs() > dayjs(shift.end))) setClosable(true);
-    }, [])
-
-    const [failing, setFailing] = useState();    
+    }, [])  
 
     return (
-        <div css={taskContainer(failing, closable)} {...props}>
+        <div css={taskContainer(failing, short, closable)} {...props}>
             <div>
                 {/* Should be using next Image component but had issues with AWS S3 domain config 🤷‍♂️ */}
                 <img src={company.pictureURL} width={"100"} height={"100"}/>
                 <h3>{company.name}</h3>
             </div>
             <p>{selection.target}</p>
+            <p>{selection.status}</p>
             <p>{details.jobType}</p>
             <p>Applicants : {details.applicants}</p>
             <Context.Provider value={[setFailing]}>
                 {shifts && <Shifts shifts={shifts}/>}
             </Context.Provider>
+            {closable && <h2>CLOSABLE</h2>}
         </div>
     )
 };
 
-const taskContainer = (failing, closable) => css`
-    /* if task is closable, push it to the bottom of the list */
-    order: ${closable && '1'};
+const taskContainer = (failing, short, closable) => css`
+    /* order tasks */
+    order: ${failing ? '-3' : short ? '-2' : closable && '1'};
     padding: 1rem; 
     margin: 1rem;
     border: ${failing && '2px solid var(--red)'};
