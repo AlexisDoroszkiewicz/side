cfa2f7415ffb8b00e430be9cf1f84b7938fea16b
AlexisDoroszkiewicz <aledoro@orange.fr>
2022-06-03 07:44:10 +0200

    no server side rendering diff

--- components/Shift.tsx
+++ components/Shift.tsx
@@ -1,4 +1,4 @@
-import { Key, useContext, useLayoutEffect, useState } from "react";
+import { Key, useContext, useEffect, useState } from "react";
 import { Context } from "./Task";
 import { css } from "@emotion/react";
 import dayjs from "dayjs";
@@ -11,7 +11,12 @@ export default function Shift ({shift, index, length, ...props}) {
     const [localShort, setLocalShort] = useState(false);
     const [localClosable, setLocalClosable] = useState(false);
 
-    useLayoutEffect(() => {
+    const [startDay, setStartDay] = useState<string>();
+    const [endDay, setEndDay] = useState<string>();
+    const [startHour, setStartHour] = useState<string>();
+    const [endHour, setEndHour] = useState<string>();
+
+    useEffect(() => {
         // if not ended, update temp worker count
         if (!ended) expectedTempWorker.current += (shift.slots - shift.filledSlots);
         // if shift began, is not ended, and slots not filled set failing to true
@@ -26,20 +31,21 @@ export default function Shift ({shift, index, length, ...props}) {
         }
         // if at the last shift and expectedState unset, update state for count; counts double in development due to react18 behaviour, counting properly on the deployed version
         if (index == length -1 && expectedTempState == 0) setExpectedTempState(expectedTempWorker.current);
+
+        // set dates inside useeffect because server timezone is different and creates server/client rendering differences
+        setStartDay(dayjs(shift.start).format('DD/MM/YYYY'));
+        setEndDay(dayjs(shift.end).format('DD/MM/YYYY'));
+
+        setStartHour(dayjs(shift.start).format('HH:mm'));
+        setEndHour(dayjs(shift.end).format('HH:mm'));
     }, [])
-
-    const startDay = dayjs(shift.start).format('DD/MM/YYYY');
-    const endDay = dayjs(shift.end).format('DD/MM/YYYY');
-
-    const startHour = dayjs(shift.start).format('HH:mm');
-    const endHour = dayjs(shift.end).format('HH:mm');
 
     return (
         <div css={shiftContainer(ended, localFailing, localShort, filled)} {...props}>
-            {/* <small>
+            <small>
                 <p css={css`font-weight: 500;`}>{startDay == endDay ? startDay : `Du ${startDay} au ${endDay}`}</p>
                 <p>{startHour} <span css={css`color: var(--grey);`}>{`>`}</span> {endHour}</p>
-            </small> */}
+            </small>
             <small css={css`display: flex; align-items: center;`}>
                 <p><span css={css`color: var(--grey);`}>Filled slots : </span> {shift.filledSlots}/{shift.slots}</p> {filled ? 
                 <svg css={css`fill: var(--green);`} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path id="checkmark-a" d="M10.67 15.82l-3.48-3.39c-.25-.23-.25-.45 0-.68l.68-.69c.25-.23.49-.23.71 0l2.43 2.4 5.4-5.29c.23-.23.47-.23.72 0l.68.65c.25.23.25.46 0 .69l-6.46 6.34c-.23.2-.46.2-.68-.03z"></path></svg> : 
